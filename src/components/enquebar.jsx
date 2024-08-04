e6cf295f934bc0e480770dbf24bce6f047d1ea21
Rabban Mehamood <151042692+RabbanMehamood@users.noreply.github.com>
2024-08-04 10:19:03 +0000

    comment

--- src/components/enquebar.jsx
+++ src/components/enquebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function EnqueBar() {
   return (
-    <div role="alert p-5">
+    <div>
       <h4 class="alert-title fw-bolder  ">City not found!</h4>
       <div class="text-secondary fw-bold ">Please refresh and try</div>
     </div>
